2a36936dd17d16c060d073bc74e9b2fb15c7a3b5
ashikur-rahman-10 <ashikur.rahman3912@gmail.com>
2023-06-12 14:03:11 +0600

    exta section with annimation

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import TopBanner from "./TopBanner/TopBanner";
 import PopulerClass from "./PopulerClass/PopulerClass";
 import PopulerInstructors from "./PopulerInstructors/PopulerInstructors";
+import JoinCommunity from "../JoinCommunity/JoinCommunity";
 
 const Home = () => {
     window.scrollTo({
@@ -14,6 +15,7 @@ const Home = () => {
             <TopBanner></TopBanner>
             <PopulerClass></PopulerClass>
             <PopulerInstructors></PopulerInstructors>
+            <JoinCommunity></JoinCommunity>
         </div>
     );
 };
---
+++ src/Pages/Home/JoinCommunity/JoinCommunity.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./JoinCommunity.css";
+import UseAxiosSecure from "../../../Hooks/UseAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+const JoinCommunity = () => {
+    const [axiosSecure] = UseAxiosSecure();
+    const { data: users = [], refetch } = useQuery(["users"], async () => {
+        const res = await axiosSecure.get("/users");
+        return res.data;
+    });
+    console.log(users);
+    const student = users.filter((user) => user.role == "Student");
+    console.log(student);
+    return (
+        <div className="join mb-20">
+            <div className="flex flex-col items-center space-y-5">
+                <h1 className="text-3xl md:text-5xl md:w-fit w-72 text-center font-semibold">
+                    Trusted by Over {student.length}+ Students{" "}
+                </h1>
+                <p className="text-xs  w-72 text-center md:w-fit md:text-lg font-medium">
+                    Join our community of students around the world helping you
+                    succeed.
+                </p>
+                <div className="relative w-[180px]">
+                    <button className="bg-[#b80924] px-5 py-3 rounded-lg font-semibold hover:text-[#b80924]  w-full z-20 absolute hover:bg-white">
+                        Join Community
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default JoinCommunity;
